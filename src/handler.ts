0a27b2f3f5a945e292268b78ef101ebd80565337
tignear <tignear+m@gmail.com>
2021-08-12 22:37:20 +0900

    update

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,15 @@
+function buildRequest(url: string, blob: Blob, ed25519: string, timestamp: string) {
+  const headers = new Headers();
+  headers.append("X-Signature-Ed25519", ed25519)
+  headers.append("X-Signature-Timestamp", timestamp);
+  headers.append("Content-Type", "application/json");
+  return new Request(url, {
+    method: "POST",
+    body: blob,
+    headers
+  });
+}
+
 export async function handleRequest(request: Request): Promise<Response> {
   if (request.method !== "POST") {
     return new Response("invalid method", {
@@ -7,19 +19,18 @@ export async function handleRequest(request: Request): Promise<Response> {
   const ed25519 = request.headers.get("X-Signature-Ed25519");
   const timestamp = request.headers.get("X-Signature-Timestamp");
   const url = request.headers.get("X-Forward");
+
   if (!(url && ed25519 && timestamp)) {
     return new Response("invalid request", {
       status: 400
     });
   }
-  const blob = await request.blob();
+  const request_blob = await request.blob();
+  const res = await fetch(buildRequest(url, request_blob, ed25519, timestamp));
+  const response_blob = await res.blob();
   const headers = new Headers();
-  headers.append("X-Signature-Ed25519", ed25519)
-  headers.append("X-Signature-Timestamp", timestamp);
-  headers.append("Content-Type", "application/json");
-  const res = await fetch(url, {
-    body: blob,
+  headers.append("X-Status", String(res.status));
+  return new Response(response_blob, {
     headers
   });
-  return new Response(await res.blob());
 }
